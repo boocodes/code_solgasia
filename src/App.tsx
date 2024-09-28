2eb1c87cea6b90b73fdfbd0dcb87ff9f150c0494
Nick Felker <felker.nick@bk.ru>
2024-09-28 20:42:41 +0600

    upload bade

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import {
+  HomePage
+} from './'
+
+  
+
+interface Props{
+
+}
+
+function App(props:Props) {
+  return(<HomePage/>)
+            
+}
+
+ 
+
+
+
+export default App;
+
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+interface IProps
+{
+
+
+}
+
+
+
+function HomePage(props:IProps)
+{
+    return(
+        <>
+        </>
+    )
+
+}
+
+
+
+export default HomePage;
