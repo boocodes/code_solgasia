d005004adc3ffd760d629c1755a801cfc03b47dd
Nick Felker <felker.nick@bk.ru>
2024-09-30 16:27:35 +0600

    external server control display info file

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-    echo "Код согласия. Эндопоинты дергать с базы и в базу"
-    ?>
+include_once "./config/database.php";
+    $database = new DataBase();
+    $db_connection = $database->set_connection();
+    echo "Код согласия. Эндопоинты дергать с базы и в базу" <br/>
+    echo "Проверка состояния сервера..." . <br/>
+    if(!empty($db_connection))
+    {
+        echo "Сервер поднят. Подключение к базе данных успешно выполнено!" <br/>
+        http_response_code(200);
+    }
+    else
+    {
+        echo "Сервер работает, но подключение к базе данных не произошло!" <br/>
+        http_response_code(200);
+    }
+    return;
+?>
