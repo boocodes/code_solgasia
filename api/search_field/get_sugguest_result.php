1ecc5116863f42ffb17b6ea96497a2397d6f6008
Nick <117706748+boocodes@users.noreply.github.com>
2024-09-30 20:54:35 +0600

    Update get_sugguest_result.php

--- api/search_field/get_sugguest_result.php
+++ api/search_field/get_sugguest_result.php
@@ -2,6 +2,11 @@
 include_once "../../config/database.php";
 include_once "../../config/search_field_sugguest.php";
 header('Content-Type: text/html; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8; multipart/form-data");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 
 $database = new DataBase();
