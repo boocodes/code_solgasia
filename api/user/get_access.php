38b5ec3aad0778ed926795a66fd8968163ec96f2
Nick Felker <felker.nick@bk.ru>
2024-10-03 17:36:41 +0600

    repaired

--- api/user/get_access.php
+++ api/user/get_access.php
@@ -1,6 +1,13 @@
 <?php
 include_once "../../config/database.php";
-include_once "../../config/access_request.php";
+include_once "../../config/access_requests.php";
+
+header('Content-Type: text/html; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8; multipart/form-data");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 $database = new DataBase();
 $db_connection = $database->set_connection();
--- api/user/registrate_user.php
+++ api/user/registrate_user.php
@@ -2,6 +2,13 @@
 include_once "../../config/database.php";
 include_once "../../config/user.php";
 
+header('Content-Type: text/html; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8; multipart/form-data");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 $database = new DataBase();
 $db_connection = $database->set_connection();
 $data = json_decode(file_get_contents('php://input'), true);
