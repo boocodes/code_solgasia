006a6375a435fb07f9e21c1a7c95130ff2590aac
Nick Felker <felker.nick@bk.ru>
2024-10-05 01:04:17 +0600

    upload

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-include_once "./config/database.php";
-    $database = new DataBase();
-    $db_connection = $database->set_connection();
-    echo "Код согласия. Эндопоинты дергать с базы и в базу" . "\n";
-    echo "Проверка состояния сервера..." . "\n";
-    if(!empty($db_connection))
-    {
-        echo "Сервер поднят. Подключение к базе данных успешно выполнено!" . "\n";
-       
-    }
-    else
-    {
-        echo "Сервер работает, но подключение к базе данных не произошло!" . "\n";
- 
-    }
-    return;
+
 ?>
