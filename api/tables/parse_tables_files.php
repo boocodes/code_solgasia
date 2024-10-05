d59d17652976485dd8bc16dfb1ca2e8c2234cd25
Nick Felker <felker.nick@bk.ru>
2024-10-05 14:41:38 +0600

    dd

--- api/tables/parse_tables_files.php
+++ api/tables/parse_tables_files.php
@@ -5,7 +5,8 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-$spreadsheet = $reader->load("test.xlsx");
+$files_path = "../../../../";
+$spreadsheet = $reader->load($files_path . "test.xlsx");
 $sheet = $spreadsheet->getSheet(0);
 
 echo json_encode($sheet->getCell("A1")->getValue());
